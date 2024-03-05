8b81678523921c0875960d2329b58c4d185f38fe
JoseRamonRenteroJimenez <jrentero@ucm.es>
2024-03-05 10:06:11 +0100

    Corrección rutas config.php. Simplificación footer y header. Corrección plantilla css.

--- Practica2/includes/vistas/comun/footer.php
+++ Practica2/includes/vistas/comun/footer.php
@@ -1,13 +1,11 @@
 <footer>
   
-        <p>Descubre tu nuevo destino!</p>
-        <p>Pueblo Innova © 2024</p>
-        <nav>
-            <ul>
-                <li><a href="soporte.php">Soporte</a></li>
-                <li><a href="faq.php">FAQ</a></li>
-            </ul>
-        </nav>
-    </footer>
-</body>
-</html>
+    <p>Descubre tu nuevo destino!</p>
+    <p>Pueblo Innova © 2024</p>
+        
+    <ul>
+        <li><a href="soporte.php">Soporte</a></li>
+        <li><a href="faq.php">FAQ</a></li>
+    </ul>
+        
+</footer>
---
+++ Practica2/porAdaptar/header.php
@@ -0,0 +1,28 @@
+
+// SOY CONSCIENTE DE QUE ESTO AHORA MISMO NO FUNCIONA
+/*
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pueblo Innova</title>
+    <!-- Aquí van los enlaces a CSS y los metadatos necesarios -->
+</head>
+<body>
+    <header>
+        <!-- Aquí va el contenido de la cabecera (logo, menú de navegación, botones de inicio de sesión) -->
+        <!-- Por ejemplo: -->
+        <nav>
+            <ul>
+                <li><a href="descubre-pueblos.php">Descubre pueblos</a></li>
+                <li><a href="explora.php">Explora</a></li>
+                <li><a href="acerca-de.php">Acerca de</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
+            </ul>
+            <div class="botones">
+                <button onclick="location.href='login.php'">Iniciar sesión</button>
+                <button onclick="location.href='registrarse.php'">Registrarse</button>
+            </div>
+        </nav>
+    </header>
+</html>*/
