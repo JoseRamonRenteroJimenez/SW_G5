433760a2077b22932d66818b1edf62e9270ab867
JoseRamonRenteroJimenez <jrentero@ucm.es>
2024-03-05 12:48:34 +0100

    Inicio login

--- Practica2/login.php
+++ Practica2/login.php
@@ -1,31 +1,14 @@
-<?php session_start() ?>
+<?php
+require_once 'includes/config.php';
+require_once 'includes/vistas/helpers/login.php';
 
-<!DOCTYPE html>
-<html>
-<body>
 
-<div id="contenedor"> <!-- Inicio del contenedor -->
+$tituloPagina = 'Login';
 
-    <main>
-        <article>
-            <h1> Iniciar Sesión </h1>
-            <form action="procesarLogin.php" method="post"> <!-- Los datos del formulario se van a enviar a ese php, "post" es para pasar datos sensibles -->
-            <div class="Nombre de usuario:">
-                <label for="username"> Usuario:</label> <!-- Campo usuario para la identificacion del usuario  -->
-                <input type="text" id="username" name="username" required> <!-- Required se pone porque esos huecos de completar texto no pueden quedar vacios -->
-            </div>
-            <div class="Contraseña">
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
+$htmlFormLogin = buildFormularioLogin();
+$contenidoPrincipal=<<<EOS
+<h1>Acceso al sistema</h1>
+$htmlFormLogin
+EOS;
 
-        <input type ="submit" value="Enviar">
-        <input type="reset" value="Reset">
-            </form>
-        </article>
-    </main>
-    
-</div> <!-- Fin del contenedor -->
-
-</body>
-</html>
+require 'includes/vistas/comun/layout.php';
