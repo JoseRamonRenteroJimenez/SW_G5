abd6c212828e7dfc7d869b2b1fa2955a1a5dac50
JoseRamonRenteroJimenez <jrentero@ucm.es>
2024-03-27 21:09:35 +0100

    Inicio formulario registro

--- Practica3/includes/formularios/FormularioRegistro.php
+++ Practica3/includes/formularios/FormularioRegistro.php
@@ -1,6 +1,11 @@
 <?php
 namespace es\ucm\fdi\aw;
 
+require_once 'Formulario.php'; // Incluir la clase base Formulario si no se ha incluido ya
+require_once __DIR__.'/../../includes/clases/Usuario.php'; // Ruta correcta hacia Usuario.php
+
+
+
 class FormularioRegistro extends Formulario
 {
     public function __construct() {
